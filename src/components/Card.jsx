bb71008b6f97795700cc2de0ef83f644f2f41102
alvaro <alvarososafrancisco1432@gmail.com>
2025-06-01 23:48:35 +0000

    add button search

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export const Card = ({ id, name, image }) => {
   return (
     <div
       key={id}
-      className=" border-cyan-500 border rounded-md p-4 flex flex-col items-center justify-around sm:w-75  md:w-auto h-auto mx-auto m-10"
+      className=" border-cyan-500 border rounded-md p-4 flex flex-col items-center justify-around sm:w-75  md:w-auto h-auto  mx-auto-10"
     >
       <img className="rounded-md w-60 md:w-90 " src={image} alt={name} />
       <h3 className="text-xl text-center p-3 text-cyan-500">{name}</h3>
--- src/pages/CharacterList.jsx
+++ src/pages/CharacterList.jsx
@@ -1,8 +1,11 @@
 import { Card } from "../components/Card";
 import { useAllCharacters } from "../hooks/useGetCharacter";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const CharacterList = () => {
+  const [search, setSearch] = useState("");
+
   const navigate = useNavigate();
   const { characters, error, loading } = useAllCharacters();
 
@@ -23,10 +26,22 @@ export const CharacterList = () => {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 m-5">
-      {characters.map((character) => (
-        <Card key={character.id} {...character} />
-      ))}
+    <div>
+      <div className="flex justify-center items-center gap-4  ">
+        <input
+          className="p-2 my-4 border border-cyan-500 rounded-md outline-none"
+          type="text"
+          placeholder="Buscar personaje"
+        />
+        <button className="bg-cyan-500 p-2 w-auto text-white text-center rounded-md cursor-pointer ">
+          Buscar personajes
+        </button>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2 mx-5">
+        {characters.map((character) => (
+          <Card key={character.id} {...character} />
+        ))}
+      </div>
     </div>
   );
 };
